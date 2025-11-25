400c2a753d2e949ca7a2d303ccbf6ba36cf29ecf
Joginder Mikael <jogindermikael@gmail.com>
2025-11-26 00:14:57 +0300

    Initial Commit. The Ecom back end.

---
+++ src/main/java/git/joginderMikael/ecom_proj/model/Product.java
@@ -0,0 +1,33 @@
+package git.joginderMikael.ecom_proj.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data //lombok automatically gets you constructor, getters and setters
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+
+
+
+}
