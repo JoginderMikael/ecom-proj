e408335ee3ada6423fd38e23a1de73460a420bc1
Joginder Mikael <jogindermikael@gmail.com>
2025-11-26 14:18:52 +0300

    Update and Delete

--- src/main/java/git/joginderMikael/ecom_proj/service/ProductService.java
+++ src/main/java/git/joginderMikael/ecom_proj/service/ProductService.java
@@ -5,7 +5,9 @@ import git.joginderMikael.ecom_proj.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Component
@@ -21,5 +23,23 @@ public class ProductService {
 
     public Product getProductById(int id) {
         return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
     }
 }
