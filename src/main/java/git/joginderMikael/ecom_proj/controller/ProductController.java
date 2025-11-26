f9677c22fa74dde40eb4d5d1eaeca73978d3d377
Joginder Mikael <jogindermikael@gmail.com>
2025-11-26 10:44:39 +0300

    Fetch a single product

--- src/main/java/git/joginderMikael/ecom_proj/controller/ProductController.java
+++ src/main/java/git/joginderMikael/ecom_proj/controller/ProductController.java
@@ -4,10 +4,7 @@ package git.joginderMikael.ecom_proj.controller;
 import git.joginderMikael.ecom_proj.model.Product;
 import git.joginderMikael.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,14 +16,13 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-
-    @RequestMapping("/")
-    public String greet(){
-        return "hello there";
-    }
-
     @GetMapping("/products")
     public List<Product> getAllProducts(){
         return service.getAllProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable int id){
+        return service.getProductById(id);
     }
 }
