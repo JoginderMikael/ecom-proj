f9681f3159697dcb168fd6f4150a6db940ae699c
Joginder Mikael <jogindermikael@gmail.com>
2025-11-26 14:19:03 +0300

    Typo fixes

--- src/main/java/git/joginderMikael/ecom_proj/model/Product.java
+++ src/main/java/git/joginderMikael/ecom_proj/model/Product.java
@@ -2,10 +2,7 @@ package git.joginderMikael.ecom_proj.model;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,18 +19,22 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private String brand;
     private BigDecimal price;
     private String category;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy")
     private Date releaseDate;
-    private boolean available;
-    private int quantity;
+    private Boolean productAvailable;
+    private Integer stockQuantity;
 
+    private String imageName;
+    private String imageType;
 
+    @Lob
+    private byte[] imageData;
 
 }
