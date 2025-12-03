deb0143197863ff2d72db63dbea9cda19bcb3a61
Joginder Mikael <jogindermikael@gmail.com>
2025-12-03 17:32:53 +0300

    mapping variable added

--- src/main/java/git/joginderMikael/ecom_proj/controller/EcomUsersController.java
+++ src/main/java/git/joginderMikael/ecom_proj/controller/EcomUsersController.java
@@ -5,13 +5,11 @@ import git.joginderMikael.ecom_proj.model.EcomUsers;
 import git.joginderMikael.ecom_proj.service.EcomUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin //handle CORS Error
+@RequestMapping("/api/user")
 public class EcomUsersController {
 
     @Autowired
