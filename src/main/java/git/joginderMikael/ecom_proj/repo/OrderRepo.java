961cf24bca555a2c43ddbb2706fef00c83098ebe
Joginder Mikael <jogindermikael@gmail.com>
2025-12-03 17:42:50 +0300

    Typo fixes and Annotation addition

--- src/main/java/git/joginderMikael/ecom_proj/repo/OrderRepo.java
+++ src/main/java/git/joginderMikael/ecom_proj/repo/OrderRepo.java
@@ -2,6 +2,8 @@ package git.joginderMikael.ecom_proj.repo;
 
 import git.joginderMikael.ecom_proj.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepo extends JpaRepository<Order, Long> {
 }
