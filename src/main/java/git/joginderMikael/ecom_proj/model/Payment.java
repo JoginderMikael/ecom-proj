7ff1356bd3529e0fa695ed4ae5933d224a1883df
Joginder Mikael <jogindermikael@gmail.com>
2025-12-03 17:19:07 +0300

    implementing payment and populating payment method enum

---
+++ src/main/java/git/joginderMikael/ecom_proj/model/Payment.java
@@ -0,0 +1,26 @@
+package git.joginderMikael.ecom_proj.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod method;
+
+    private boolean paid;
+
+    private LocalDateTime paidAt;
+}
---
+++ src/main/java/git/joginderMikael/ecom_proj/model/PaymentMethod.java
@@ -0,0 +1,8 @@
+package git.joginderMikael.ecom_proj.model;
+
+public enum PaymentMethod {
+    MPESA,
+    CASH_ON_DELIVERY,
+    CARD,
+    PAYPAL
+}
