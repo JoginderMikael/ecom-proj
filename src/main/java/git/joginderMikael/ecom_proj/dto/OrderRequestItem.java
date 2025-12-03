9935b6a63306fe90a3c3b8ba61dde637bc9292f4
Joginder Mikael <jogindermikael@gmail.com>
2025-12-03 18:15:22 +0300

    added lambok

--- src/main/java/git/joginderMikael/ecom_proj/dto/OrderRequestItem.java
+++ src/main/java/git/joginderMikael/ecom_proj/dto/OrderRequestItem.java
@@ -1,5 +1,12 @@
 package git.joginderMikael.ecom_proj.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequestItem {
     private Long productId;
     private int quantity;
