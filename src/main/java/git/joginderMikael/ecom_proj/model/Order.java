c84f8ce24c0f99c93d14ba63e0626839ffc71930
Joginder Mikael <jogindermikael@gmail.com>
2025-12-03 17:07:38 +0300

    Order Entity implementation

---
+++ src/main/java/git/joginderMikael/ecom_proj/model/Order.java
@@ -0,0 +1,51 @@
+package git.joginderMikael.ecom_proj.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private EcomUsers user;
+
+    private LocalDate createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shipping_id")
+    private ShippingAddress shippingAddress;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "payment_id")
+    private Payment payment;
+
+    private double totalAmount;
+
+    //for convenience
+    public void addItem(OrderItem item){
+        items.add(item);
+        item.setOrder(this);
+    }
+}
