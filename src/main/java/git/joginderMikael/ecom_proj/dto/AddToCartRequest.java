2c0e8e84d3e831fc8263a7fe167cdacede9b5f91
Joginder Mikael <jogindermikael@gmail.com>
2025-12-04 23:10:14 +0300

    Adding a dto for cart request

---
+++ src/main/java/git/joginderMikael/ecom_proj/dto/AddToCartRequest.java
@@ -0,0 +1,14 @@
+package git.joginderMikael.ecom_proj.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddToCartRequest {
+    private Long productId;
+    private int quantity;
+}
