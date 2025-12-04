57b2a9d261dc8a8077584625e2a5854c5b4f1683
Joginder Mikael <jogindermikael@gmail.com>
2025-12-04 23:10:35 +0300

    All relevant repo for manipulation of car

---
+++ src/main/java/git/joginderMikael/ecom_proj/controller/CartController.java
@@ -0,0 +1,23 @@
+package git.joginderMikael.ecom_proj.controller;
+
+import git.joginderMikael.ecom_proj.dto.AddToCartRequest;
+import git.joginderMikael.ecom_proj.model.Cart;
+import git.joginderMikael.ecom_proj.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController()
+@RequestMapping("/api/cart")
+public class CartController {
+
+    @Autowired
+    CartService cartService;
+    @PostMapping("/add")
+    public ResponseEntity<Cart> addToCart(@RequestBody AddToCartRequest addToCartRequest){
+        Cart cart = cartService.addToCart(addToCartRequest);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/git/joginderMikael/ecom_proj/service/CartService.java
@@ -0,0 +1,12 @@
+package git.joginderMikael.ecom_proj.service;
+
+import git.joginderMikael.ecom_proj.dto.AddToCartRequest;
+import git.joginderMikael.ecom_proj.model.Cart;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+    public Cart addToCart(AddToCartRequest addToCartRequest) {
+        return null;
+    }
+}
