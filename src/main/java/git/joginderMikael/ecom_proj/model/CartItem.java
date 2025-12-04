37d9ea5bebb7153c7027be8cb5f20591a4893a3d
Joginder Mikael <jogindermikael@gmail.com>
2025-12-04 21:58:07 +0300

    Added cart Item entity

---
+++ src/main/java/git/joginderMikael/ecom_proj/model/CartItem.java
@@ -0,0 +1,28 @@
+package git.joginderMikael.ecom_proj.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+    private double priceAtAdd;
+}
