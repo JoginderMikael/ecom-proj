8cf6cc6c6a16b1c0d1389902ff187279ec67ea6c
Joginder Mikael <jogindermikael@gmail.com>
2025-12-04 21:58:26 +0300

    Added lambok annotations

--- src/main/java/git/joginderMikael/ecom_proj/model/Cart.java
+++ src/main/java/git/joginderMikael/ecom_proj/model/Cart.java
@@ -1,10 +1,18 @@
 package git.joginderMikael.ecom_proj.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
 
     @Id
