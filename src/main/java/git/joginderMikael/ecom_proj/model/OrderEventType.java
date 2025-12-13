3947200f74b9d89f2ec6f027cb65a2498da8fdc7
Joginder Mikael <jogindermikael@gmail.com>
2025-12-14 00:19:40 +0300

    Order eventTypes entity implementation.

---
+++ src/main/java/git/joginderMikael/ecom_proj/model/OrderEventType.java
@@ -0,0 +1,11 @@
+package git.joginderMikael.ecom_proj.model;
+
+public enum OrderEventType {
+    ORDER_CREATED,
+    ORDER_COMPLETED,
+    ORDER_FAILED,
+    ORDER_CHANGED,
+    ORDER_SHIPPED,
+    ORDER_DELIVERED,
+    ORDER_CANCELLED
+}
