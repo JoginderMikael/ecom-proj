ea2eae0e6306e3adcb1fee2358185803be9e7a2d
Joginder Mikael <jogindermikael@gmail.com>
2025-12-14 00:15:42 +0300

    Order events entity implementation.

---
+++ src/main/java/git/joginderMikael/ecom_proj/model/OrderEvent.java
@@ -0,0 +1,33 @@
+package git.joginderMikael.ecom_proj.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    private OrderEventType eventType;
+
+    private String message;
+
+    private LocalDateTime createdAt;
+}
